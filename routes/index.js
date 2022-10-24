195616d0e472a4826cd1b0e83c0b9bd201052a4e
askdominik <76659589+askdominik@users.noreply.github.com>
2022-10-24 13:08:05 +0300

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var schemas = require('../models/schemas.js');
+
+// Home page
+router.get('/', async(req, res) => {
+  let menu = schemas.menu;
+  let sesh = req.session;
+
+  let menuResult = await menu.find({}).then( (menuData) => {
+    res.render('index', {title:'Menu App', data:menuData, search:'', loggedIn:sesh.loggedIn});
+  });
+});
+
+//Logout
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+//Search box
+router.post('/q', async(req, res) => {
+  let menu = schemas.menu;
+  let q = req.body.searchInput;
+  let menuData = null;
+  let sesh = req.session;
+  let qry = {name:{$regex:'^' + q, $options:'i'}};
+
+  if (q != null) {
+    let menuResult = await menu.find(qry).then( (data) => {
+      menuData = data;
+    });
+  } else {
+    q = 'Search';
+    let menuResult = await menu.find({}).then( (data) => {
+      menuData = data;
+    });
+  }
+
+  res.render('index', {title:'Menu App', data:menuData, search:q, loggedIn:sesh.loggedIn});
+});
+
+module.exports = router;
